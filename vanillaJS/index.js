4e62b68b043eb74a3d4d8f8cd32e98448b98a222
­신희원 <shw0210@hanyang.ac.kr>
2020-03-28 14:16:43 +0000

    practice vanillaJS Theory

---
+++ vanillaJS/index.js
@@ -0,0 +1,25 @@
+/*let a=221; //declare선언
+const b=a-5; //assignment할당
+a=4;
+console.log(b,a);
+*/
+/*
+let var const differance
+let : redeclare x reassignment o
+const : x x
+var : o o
+*/
+/* String number boolean float
+const what = "Hello";
+const numb=111;
+const gender=true;
+const flo=55.1;
+ */
+/*array
+const daysOfWeek =["mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+same with python
+*/
+/* Object : same with python dictionary
+const Info={name:"sva", favfood:"sisi"}
+console.log(Info.name);
+*/
