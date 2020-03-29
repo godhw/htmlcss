471478811c1aaa6749f8dd7beefd772aa4429bf5
­신희원 <shw0210@hanyang.ac.kr>
2020-03-29 14:00:16 +0000

    DOM practice very important

--- vanillaJS/index.js
+++ vanillaJS/index.js
@@ -3,23 +3,39 @@ const b=a-5; //assignment할당
 a=4;
 console.log(b,a);
 */
+
 /*
 let var const differance
 let : redeclare x reassignment o
 const : x x
 var : o o
 */
+
 /* String number boolean float
 const what = "Hello";
 const numb=111;
 const gender=true;
 const flo=55.1;
  */
+
 /*array
 const daysOfWeek =["mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 same with python
 */
+
 /* Object : same with python dictionary
 const Info={name:"sva", favfood:"sisi"}
 console.log(Info.name);
+*/
+
+//function sayHello()
+
+/*DOM interface 매우중요!!
+const title = document.getElementById("title");
+title.innerHTML = "HI"
+
+const title = document.querySelector("#title");
+title.innerHTML = "Hi! From JS";
+title.style.color = "white";
+document.title = "I own you now";
 */
