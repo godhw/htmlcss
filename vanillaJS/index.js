a0daeb1ee47a4c16be3bfbd7e958326844e65bc9
­신희원 <shw0210@hanyang.ac.kr>
2020-03-30 13:54:35 +0000

    css control in js practice

--- vanillaJS/index.js
+++ vanillaJS/index.js
@@ -39,3 +39,24 @@ title.innerHTML = "Hi! From JS";
 title.style.color = "white";
 document.title = "I own you now";
 */
+/* CSS js로 조작하기
+const title = document.querySelector("#title");
+
+const BASE_COLOR = "blue";
+const OTHER_COLOR = "#7f8c8d";
+
+function handleClick() {
+	const currentColor = title.style.color;
+	if (currentColor === BASE_COLOR) {
+		title.style.color = OTHER_COLOR;
+	} else {
+		title.style.color = BASE_COLOR;
+	}
+}
+
+function init() {
+	title.style.color = BASE_COLOR;
+	title.addEventListener("click", handleClick);
+}
+init();
+*/
